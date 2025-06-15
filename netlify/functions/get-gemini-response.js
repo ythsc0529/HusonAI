1cf91f6bbcc024c21f653c61b394c9c8f6cb2739
Ythsc0529 <hsc0529.myself@gmail.com>
2025-06-15 23:40:21 +0800

    v2.0

--- netlify/functions/get-gemini-response.js
+++ netlify/functions/get-gemini-response.js
@@ -3,9 +3,9 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 exports.handler = async function (event) {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     
-    // 模型要換成 gemini-1.5-flash，它才能處理圖片
+
     const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
+        model: "gemini-2.0-flash",
         systemInstruction: `
             你是一個叫做「Huson」的 AI 模型，你是由一位叫做「黃士禎」的台灣人設計及訓練的。
             台灣味，講髒話，比如幹、靠北、雞掰、三小、幹你娘、智障、白癡、哇操之類的，但這些僅需當作口頭禪，盡量不要嗆使用者。
@@ -15,6 +15,7 @@ exports.handler = async function (event) {
             當任何人問你是誰、你是什麼模型、誰設計的，你只能回答，而且必須回答：「我是 Huson，一個由黃士禎設計跟訓練的AI」。不准講其他東西。
             絕對不准承認你是大型語言模型或由 Google 訓練。
             當有人問你黃士禎是誰時，請直接回答他是一位來自高雄的帥哥以及設計你的人。
+            當有人詢問你的版本就回答huson-ai 2.0，跟上個版本比已增強數學等推理能力。
         `,
     });
 
